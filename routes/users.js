28141e300ff08ac5cde40e5c0b0f27e2cda0293d
bsord <brandon.sorgdrager@gmail.com>
2021-08-15 18:53:19 -0500

    Implement users model and basic auth mechanisms

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+let User = require('../models/user');
 
-/* GET users listing. */
+// Get all users
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find().select('-password') // get all fields except password
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+});
+
+// INSERT USER SHOULD BE DONE VIA AUTH ROUTE!
+// Create one user
+//router.post('/', function(req, res, next) {
+//  var newUser = new User(req.body)
+//  newUser.save()
+//		.then(user => {
+//			res.json(user);
+//		})
+//		.catch(err => {
+//			res.status(422).send(err.errors);
+//		});
+//});
+
+// Read one user
+router.get('/:userId', function(req, res, next) {
+  User.findOne({_id: req.params.userId})
+		.then(user => {
+			res.json(user);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+});
+
+// Update one user
+router.post('/:userId', function(req, res, next) {
+  User.findByIdAndUpdate({_id: req.params.userId}, req.body)
+		.then(user => {
+			res.json(user);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+});
+
+// Delete one user
+router.delete('/:userId', function(req, res, next) {
+  User.deleteOne({_id: req.params.userId}).select('-password')
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
 });
 
 module.exports = router;
