890fa8da5a5caad8df3d98845c0cfd8fa99989df
Jon Fairbanks <fairplay89@gmail.com>
2021-08-16 16:54:58 -0700

    Fix test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../views/App';
 
-test('renders learn react link', () => {
+test('renders Uptime Monitor link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Uptime Monitor/i);
   expect(linkElement).toBeInTheDocument();
