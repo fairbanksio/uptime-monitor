f7b650722287e2bbb097aebae81491dd4bff3d35
bsord <brandon.sorgdrager@gmail.com>
2021-08-17 11:11:35 -0500

    Implement auth controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,56 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let User = require('../models/user');
-var jwt = require('jsonwebtoken');
-var settings = require('../config/settings');
+let AuthController = require('../controllers/auth');
 
-// Register a new user
-router.post('/register', function(req, res, next) {
-  var newUser = new User(req.body)
-  newUser.save() // get all fields except password
-		.then(user => {
-			res.json(user);
-		})
-		.catch(err => {
-			res.status(422).send(err.errors);
-		});
-});
-
-// Authenticate
-router.post('/login', function(req, res, next) {
-    User.findOne({
-        username: req.body.username
-    }, function (err, user) {
-        if (err) throw err;
-            if (!user) {
-                res.status(401).send({ success: false, msg: 'Authentication failed' }); //user not found
-            } else {
-                // check if password matches
-                user.comparePassword(req.body.password, function (err, isMatch) {
-                if (isMatch && !err) {
-                    // if user is found and password is right create a token
-                    var token = ""
-                    // return the information including token as JSON
-                    user.token = jwt.sign(user.toJSON(), settings.jwtSecret);
-                    res.json(user);
-                } else {
-                    res.status(401).send({ success: false, msg: err }); //wrong password
-                }
-            });
-        }
-    }).select('+password');
-});
-
-// Sign out
-router.post('/logout', function(req, res, next) {
-  User.findByIdAndUpdate({_id: req.params.userId}, req.body).select('-password') // get all fields except password
-		.then(user => {
-			res.json(user);
-		})
-		.catch(err => {
-			res.status(422).send(err.errors);
-		});
-});
-
+// Handle Auth
+router.post('/register', AuthController.register); //Register user via email/username/pass
+router.post('/login', AuthController.login); //Authenticate User/Pass
+router.post('/logout', AuthController.logout);
 
 module.exports = router;
