d4459867f09531dcc29954d4f34aa6cd13cce14e
Jon Fairbanks <fairplay89@gmail.com>
2021-08-16 18:39:44 -0700

    Update index.test.js

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,6 +1,10 @@
 const request = require('supertest')
 const app = require('../app')
 
+afterAll(async () => {
+  await new Promise((resolve) => setTimeout(() => resolve(), 500)) // avoid jest open handle error
+})
+
 describe('Verify the site loads', () => {
   test('Response should equal HTTP 200', (done) => {
     request(app)
@@ -8,6 +12,7 @@ describe('Verify the site loads', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200)
         done()
+        app.close()
       })
   })
 })
