5b73fef4ad4302b44a3927ea54725a48b30f5bdf
bsord <brandon.sorgdrager@gmail.com>
2021-08-18 21:14:27 -0500

    add monitor create unit test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,6 +6,7 @@ require('dotenv').config({ path: '.env.sample' }) // eslint-disable-line
 
 afterAll(() => mongoose.disconnect())
 
+// Test API is running
 describe('Verify the site loads', () => {
   test('Response should equal HTTP 200', (done) => {
     request(app)
@@ -17,6 +18,7 @@ describe('Verify the site loads', () => {
   })
 })
 
+// Test auth
 const user = {
   username: "testUser",
   password: "testPassword"
@@ -29,6 +31,7 @@ describe('User can register', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200)
         expect(response.body).toMatchObject({
+          _id: expect.any(String),
           username: "testUser",
           password: expect.any(String)
         })
@@ -44,6 +47,7 @@ describe('User can authenticate', () => {
       .then((response) => {
         expect(response.statusCode).toBe(200)
         expect(response.body).toMatchObject({
+          _id: expect.any(String),
           username: "testUser",
           token: expect.any(String)
         })
@@ -52,3 +56,35 @@ describe('User can authenticate', () => {
   })
 })
 
+
+// Test Monitors
+const monitor = {
+  name: "test-monitor",
+  enabled: true,
+  interval: 60,
+  type: "http",
+  config: {
+    httpUrl: "https://www.google.com"
+  }
+};
+
+describe('Monitor can be created', () => {
+  test('Response should contain monitor object plus an id', (done) => {
+    request(app)
+      .post('/api/monitors').send(monitor)
+      .then((response) => {
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toMatchObject({
+          _id: expect.any(String),
+          name: "test-monitor",
+          enabled: true,
+          interval: 60,
+          type: "http",
+          config: {
+            httpUrl: "https://www.google.com"
+          }
+        })
+        done()
+      })
+  })
+})
