cb18d96629cff3ae30d4d25dcaeb22ef1ec8212d
bsord <brandon.sorgdrager@gmail.com>
2021-08-19 21:30:30 -0500

    More fixes

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -3,6 +3,6 @@ import App from '../App';
 
 test('renders Uptime Monitor link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Uptime Monitor/i);
+  const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
