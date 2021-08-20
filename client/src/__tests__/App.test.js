d9cea66357af7c6ddc14039cc1e89e7371b5fb6d
bsord <brandon.sorgdrager@gmail.com>
2021-08-19 21:25:24 -0500

    fix client test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../views/App';
+import App from './views/App';
 
 test('renders Uptime Monitor link', () => {
   render(<App />);
