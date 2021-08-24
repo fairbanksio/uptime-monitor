55dfef6501c4156dd1ee8655371924ff902ff14a
bsord <brandon.sorgdrager@gmail.com>
2021-08-24 01:42:29 -0500

    Limit returned heartbeats

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,4 +1,4 @@
-let Monitor = require('../models/monitor')
+let Monitor = require('../models/monitor');
 var monitoringService = require('../services/monitoring')
 
 // Get all monitors
@@ -14,44 +14,9 @@ exports.getAll = (req, res, next) => {
 
 // Create monitor
 exports.create = (req, res, next) => {
-  const { name, interval, enabled, type, config, notifications } = req.body
-  const { user } = req
-  var newMonitor = new Monitor({
-    name: name,
-    interval: interval,
-    enabled: enabled,
-    type: type,
-    config: config,
-    owner: user._id,
-    notifications: notifications,
-  })
-  newMonitor
-    .save()
-    .then((monitor) => {
-      monitoringService.startMonitor(monitor._id)
-      res.json(monitor)
-    })
-    .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
-
-// Read one monitor
-exports.getOne = (req, res, next) => {
-  Monitor.findOne({ _id: req.params.monitorId, owner: req.user._id })
-    .then((monitor) => {
-      res.json(monitor)
-    })
-    .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
-
-// Update one monitor
-exports.update = (req, res, next) => {
 	const {name, interval, enabled, type, config, notifications} = req.body
 	const {user} = req
-	var updatedMonitor = {
+	var newMonitor = new Monitor({
 		name: name,
 		interval: interval,
 		enabled: enabled,
@@ -59,10 +24,42 @@ exports.update = (req, res, next) => {
 		config: config,
 		owner: user._id,
 		notifications: notifications
-	}
-
-  Monitor.findByIdAndUpdate({_id: req.params.monitorId, owner: req.user._id}, updatedMonitor, {new: true}).populate('events heartbeats').slice('heartbeats', -20)
+	})
+	newMonitor.save()
 		.then(monitor => {
+			monitoringService.startMonitor(monitor._id)
+			res.json(monitor);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+};
+
+// Read one monitor
+exports.getOne = (req, res, next) => {
+  Monitor.findOne({_id: req.params.monitorId, owner: req.user._id})
+		.then(monitor => {
+			res.json(monitor);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+};
+
+// Update one monitor
+exports.update = (req, res, next) => {
+
+	const updatedMonitor = query => ({
+		...query.name && { name: query.name },
+		...query.interval && { interval: query.interval },
+		...query.enabled && { enabled: query.enabled },
+		...query.config && { config: query.config },
+		...query.owner && { owner: query.owner },
+		...query.notifications && { notifications: query.notifications },
+	});
+
+  Monitor.findByIdAndUpdate({_id: req.params.monitorId, owner: req.user._id}, updatedMonitor(req.body), {new: true}).populate('events heartbeats').slice('heartbeats', -10)
+		.then(monitor => { 
 			monitoringService.updateMonitor(monitor._id)
 			res.json(monitor);
 		})
@@ -71,24 +68,24 @@ exports.update = (req, res, next) => {
 		});
 };
 
-
 // Delete one monitor
 exports.delete = (req, res, next) => {
-  // delete doesn't return an object id and we need it to stop monitor
-  Monitor.findOne({ _id: req.params.monitorId, owner: req.user._id })
-    .then((monitor) => {
-      monitoringService.stopMonitor(monitor._id)
-    })
-    .catch((err) => {
-      res.status(422).send(err.errors)
-    })
+	// delete doesn't return an object id and we need it to stop monitor
+	Monitor.findOne({_id: req.params.monitorId, owner: req.user._id})
+		.then(monitor => {
+			monitoringService.stopMonitor(monitor._id)
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
 
-  // delete monitor
-  Monitor.deleteOne({ _id: req.params.monitorId })
-    .then((deleteResult) => {
-      res.json(deleteResult)
-    })
-    .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+	// delete monitor
+	Monitor.deleteOne({_id: req.params.monitorId})
+		.then(deleteResult => {
+			res.json(deleteResult);
+		})
+		.catch(err => {
+			res.status(422).send(err.errors);
+		});
+};
+
