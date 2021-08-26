ac6a2e913a926fde1349d2b94dbb9dfe1b18ffa3
Jon Fairbanks <fairplay89@gmail.com>
2021-08-26 14:24:30 -0700

    Handle no heartbeats

--- client/src/components/Monitor/MonitorHeartbeats.js
+++ client/src/components/Monitor/MonitorHeartbeats.js
@@ -15,19 +15,26 @@ function MonitorHeartbeats(monitor) {
     <div>
       Heartbeats:
       <div style={{ maxHeight: 200, overflowX: 'auto' }}>
-        {heartbeats.map((heartbeat, key) => (
-          <div key={key}>
-            {heartbeat.status === 'UP' ? (
-              <FontAwesomeIcon icon={faCircle} style={{ color: 'green' }} />
-            ) : (
-              <FontAwesomeIcon icon={faCircle} style={{ color: 'red' }} />
-            )}{' '}
-            {moment(heartbeat.createdAt).fromNow()}
-            <pre style={{ float: '' }}>
-              {heartbeat.statusMessage + ' in ' + heartbeat.responseTime + 'ms'}
-            </pre>{' '}
-          </div>
-        ))}
+        {heartbeats.length > 0 ? (
+          heartbeats.map((heartbeat, key) => (
+            <div key={key}>
+              {heartbeat.status === 'UP' ? (
+                <FontAwesomeIcon icon={faCircle} style={{ color: 'green' }} />
+              ) : (
+                <FontAwesomeIcon icon={faCircle} style={{ color: 'red' }} />
+              )}{' '}
+              {moment(heartbeat.createdAt).fromNow()}
+              <pre style={{ float: '' }}>
+                {heartbeat.statusMessage +
+                  ' in ' +
+                  heartbeat.responseTime +
+                  'ms'}
+              </pre>{' '}
+            </div>
+          ))
+        ) : (
+          <div>No recent heartbeats</div>
+        )}
       </div>
     </div>
   )
