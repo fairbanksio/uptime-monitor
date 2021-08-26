ef0457cd7a859ec4748f2179612637ace54ccaf9
bsord <brandon.sorgdrager@gmail.com>
2021-08-26 19:55:03 +0000

    Initial google auth implementation on backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var router = express.Router()
 let AuthController = require('../../controllers/auth')
+var passport = require('passport');
 
 // Handle Auth
 router.post('/register', AuthController.register) //Register user via email/username/pass
 router.post('/login', AuthController.login) //Authenticate User/Pass
 router.post('/logout', AuthController.logout)
+router.post('/google', passport.authenticate('google-token', { session: false}), AuthController.loginGoogle); //Authenticate Google Token/oAuth2
 
 module.exports = router
