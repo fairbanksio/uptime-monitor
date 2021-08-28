838a4002e6fe170c75603a6a380bfb09dcec5af2
Jon Fairbanks <fairplay89@gmail.com>
2021-08-28 02:29:37 -0700

    Input validation on register

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -13,6 +13,9 @@ function Register() {
     username: '',
     password: '',
   })
+  const [invalidUser, isInvalidUser] = React.useState(false)
+  const [invalidPassword, isInvalidPassword] = React.useState(false)
+  const [validForm, isFormValid] = React.useState(false)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target
@@ -25,38 +28,54 @@ function Register() {
     }
   }
 
+  const verifyForm = () => {
+    isFormValid(false) // Reset the state for people having to retry
+    if (loginInfo.username && loginInfo.username.length > 1) {
+      isInvalidUser(false)
+    } else isInvalidUser(true)
+    if (loginInfo.password && loginInfo.password.length > 1) {
+      isInvalidPassword(false)
+    } else isInvalidPassword(true)
+    if (invalidUser === false && invalidPassword === false) {
+      isFormValid(true)
+    }
+  }
+
   const registerUser = () => {
-    register(registerInfo.username, registerInfo.password, (result) => {
-      if (result && result.status === 'success') {
-        const id = 'register-success-toast'
-        if (!toast.isActive(id)) {
-          toast({
-            title: 'Account created.',
-            description: "We've created your account for you.",
-            status: 'success',
-            variant: 'subtle',
-            duration: 9000,
-            isClosable: true,
-          })
+    verifyForm()
+    if (validForm === true) {
+      register(registerInfo.username, registerInfo.password, (result) => {
+        if (result && result.status === 'success') {
+          const id = 'register-success-toast'
+          if (!toast.isActive(id)) {
+            toast({
+              title: 'Account created.',
+              description: "We've created your account for you.",
+              status: 'success',
+              variant: 'subtle',
+              duration: 9000,
+              isClosable: true,
+            })
+          }
+          setTimeout(() => {
+            history.push('/')
+          }, 2000)
+        } else {
+          const id = 'register-error-toast'
+          if (!toast.isActive(id)) {
+            toast({
+              id,
+              title: 'An error occurred.',
+              description: 'Unable to create user account.',
+              status: 'error',
+              variant: 'subtle',
+              duration: 9000,
+              isClosable: true,
+            })
+          }
         }
-        setTimeout(() => {
-          history.push('/')
-        }, 2000)
-      } else {
-        const id = 'register-error-toast'
-        if (!toast.isActive(id)) {
-          toast({
-            id,
-            title: 'An error occurred.',
-            description: 'Unable to create user account.',
-            status: 'error',
-            variant: 'subtle',
-            duration: 9000,
-            isClosable: true,
-          })
-        }
-      }
-    })
+      })
+    }
   }
 
   return (
@@ -71,6 +90,7 @@ function Register() {
         placeholder="username"
         size="md"
         width={'300'}
+        isInvalid={invalidUser}
       />
       <br />
       <Input
@@ -84,6 +104,7 @@ function Register() {
         placeholder="password"
         size="md"
         width={'300'}
+        isInvalid={invalidPassword}
       />
 
       <br />
