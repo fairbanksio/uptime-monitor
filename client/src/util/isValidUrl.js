311961ba0b0b9b23ca3d94b6301a6021c44b4e26
Jon Fairbanks <fairplay89@gmail.com>
2021-08-29 00:44:18 -0700

    Notification Input Validation

---
+++ client/src/util/isValidUrl.js
@@ -0,0 +1,8 @@
+const isValidUrl = (string) => {
+  var res = string.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  )
+  return res !== null
+}
+
+export default isValidUrl
