db09da119fa105248a8310683bb6c93baa978de5
Jon Fairbanks <fairplay89@gmail.com>
2021-09-01 15:23:02 -0700

    Enable React Table features

--- client/src/components/Monitor/MonitorEvents.js
+++ client/src/components/Monitor/MonitorEvents.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import {
   Center,
+  Input,
+  Select,
   Table,
   Thead,
   Tbody,
@@ -8,7 +10,10 @@ import {
   Tr,
   Th,
   Td,
+  chakra,
 } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { useTable, useSortBy, usePagination } from 'react-table'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import moment from 'moment-timezone'
@@ -59,65 +64,261 @@ function MonitorEvents(monitor) {
   const thisMonitor = monitors.filter(function (el) {
     return el._id === monitor.monitor._id
   })[0]
-  const events = thisMonitor.events
+
   const userTZ = moment.tz.guess(true) // If set to true, caching will be ignored and overwritten with the new value.
-  return (
-    <Table size="sm" variant="simple">
-      <Thead>
-        <Tr>
-          <Th>Type</Th>
-          <Th>Timestamp</Th>
-          <Th>Status</Th>
-          <Th style={{ width: '150px' }}>Event Duration</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {events.length > 0 ? (
-          events.map((event, key) => (
-            <Tr key={key}>
-              <Td style={{ paddingLeft: '20px' }}>
-                {event.type && event.type === 'UP' ? (
-                  <FontAwesomeIcon
-                    icon={faArrowUp}
-                    style={{ color: 'green' }}
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={faArrowDown}
-                    style={{ color: 'red' }}
-                  />
-                )}
-              </Td>
-              <Td style={{ fontSize: '12px', width: '175px' }}>
-                {moment(event.createdAt).tz(userTZ).format('MMM Do, h:mm:ss a')}
-              </Td>
-              <Td>
-                <code>{event.message}</code>
-              </Td>
-              <Td style={{ fontSize: '12px' }}>
-                {splitDuration(event.duration)}
-              </Td>
-            </Tr>
-          ))
-        ) : (
-          <Tr key={'no-recent-events'}>
-            <Td colspan="4">
-              <Center>No recent events</Center>
-            </Td>
-          </Tr>
-        )}
-      </Tbody>
-      <Tfoot>
-        <Tr>
-          <Th>Type</Th>
-          <Th>Timestamp</Th>
-          <Th>Status</Th>
-          <Th>Event Duration</Th>
-        </Tr>
-      </Tfoot>
-      {/* <TableCaption>Recently Detected Events</TableCaption> */}
-    </Table>
+  const data = thisMonitor.events
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Type',
+        accessor: 'type',
+        Cell: (cellProps) => {
+          if (cellProps.value === 'UP') {
+            return (
+              <FontAwesomeIcon
+                {...cellProps}
+                icon={faArrowUp}
+                style={{ color: 'green', marginLeft: '7px' }}
+              />
+            )
+          } else {
+            return (
+              <FontAwesomeIcon
+                {...cellProps}
+                icon={faArrowDown}
+                style={{ color: 'red', marginLeft: '7px' }}
+              />
+            )
+          }
+        },
+      },
+      {
+        Header: 'Timestamp',
+        accessor: 'createdAt',
+        Cell: (cellProps) => {
+          return moment(cellProps.value).tz(userTZ).format('MMM Do, h:mm:ss a')
+        },
+      },
+      {
+        Header: 'Status',
+        accessor: 'message',
+        Cell: (cellProps) => {
+          return <code>{cellProps.value}</code>
+        },
+      },
+      {
+        Header: 'Event Duration',
+        accessor: 'duration',
+        isNumeric: true,
+        Cell: (cellProps) => {
+          return <Center>{splitDuration(cellProps.value)}</Center>
+        },
+      },
+    ],
+    []
   )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 5 },
+    },
+    useSortBy,
+    usePagination
+  )
+
+  return (
+    <div>
+      <Table {...getTableProps()} size="sm" variant="simple">
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                >
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row, key) => {
+            prepareRow(row)
+            return (
+              <Tr {...row.getRowProps()} key={key}>
+                {row.cells.map((cell) => (
+                  <Td
+                    {...cell.getCellProps()}
+                    isNumeric={cell.column.isNumeric}
+                  >
+                    {cell.render('Cell')}
+                  </Td>
+                ))}
+              </Tr>
+            )
+          })}
+        </Tbody>
+        <Tfoot>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                >
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Tfoot>
+      </Table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <Input
+            type="number"
+            size="xs"
+            placeholder="page"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>
+        <div style={{ width: '100px', float: 'right' }}>
+          <Select
+            size="xs"
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[5, 10, 25, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </div>
+  )
+
+  // return (
+  //   <Table size="sm" variant="simple">
+  //     <Thead>
+  //       <Tr>
+  //         <Th>Type</Th>
+  //         <Th>Timestamp</Th>
+  //         <Th>Status</Th>
+  //         <Th style={{ width: '150px' }}>Event Duration</Th>
+  //       </Tr>
+  //     </Thead>
+  //     <Tbody>
+  //       {events.length > 0 ? (
+  //         events.map((event, key) => (
+  //           <Tr key={key}>
+  //             <Td style={{ paddingLeft: '20px' }}>
+  //               {event.type && event.type === 'UP' ? (
+  //                 <FontAwesomeIcon
+  //                   icon={faArrowUp}
+  //                   style={{ color: 'green' }}
+  //                 />
+  //               ) : (
+  //                 <FontAwesomeIcon
+  //                   icon={faArrowDown}
+  //                   style={{ color: 'red' }}
+  //                 />
+  //               )}
+  //             </Td>
+  //             <Td style={{ fontSize: '12px', width: '175px' }}>
+  //               {moment(event.createdAt).tz(userTZ).format('MMM Do, h:mm:ss a')}
+  //             </Td>
+  //             <Td>
+  //               <code>{event.message}</code>
+  //             </Td>
+  //             <Td style={{ fontSize: '12px' }}>
+  //               {splitDuration(event.duration)}
+  //             </Td>
+  //           </Tr>
+  //         ))
+  //       ) : (
+  //         <Tr key={'no-recent-events'}>
+  //           <Td colspan="4">
+  //             <Center>No recent events</Center>
+  //           </Td>
+  //         </Tr>
+  //       )}
+  //     </Tbody>
+  //     <Tfoot>
+  //       <Tr>
+  //         <Th>Type</Th>
+  //         <Th>Timestamp</Th>
+  //         <Th>Status</Th>
+  //         <Th>Event Duration</Th>
+  //       </Tr>
+  //     </Tfoot>
+  //     {/* <TableCaption>Recently Detected Events</TableCaption> */}
+  //   </Table>
+  // )
 }
 
 export default MonitorEvents
--- client/src/components/Monitor/MonitorHeartbeats.js
+++ client/src/components/Monitor/MonitorHeartbeats.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import {
   Center,
+  Input,
+  Select,
   Table,
   Thead,
   Tbody,
@@ -8,7 +10,10 @@ import {
   Tr,
   Th,
   Td,
+  chakra,
 } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { useTable, useSortBy, usePagination } from 'react-table'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
 import moment from 'moment-timezone'
@@ -20,59 +25,255 @@ function MonitorHeartbeats(monitor) {
   const thisMonitor = monitors.filter(function (el) {
     return el._id === monitor.monitor._id
   })[0]
-  const heartbeats = thisMonitor.heartbeats
-  return (
-    <Table size="sm" variant="simple">
-      <Thead>
-        <Tr>
-          <Th>Status</Th>
-          <Th>Time</Th>
-          <Th>Status Message</Th>
-          <Th isNumeric style={{ width: '150px' }}>
-            Response Time
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {heartbeats.length > 0 ? (
-          heartbeats.map((heartbeat, key) => (
-            <Tr key={key}>
-              <Td style={{ paddingLeft: '27px', width: '15px' }}>
-                {heartbeat.status === 'UP' ? (
-                  <FontAwesomeIcon icon={faCheck} style={{ color: 'green' }} />
-                ) : (
-                  <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
-                )}
-              </Td>
-              <Td style={{ fontSize: '12px' }}>
-                {moment(heartbeat.createdAt).fromNow()}
-              </Td>
-              <Td>
-                <code>{heartbeat.statusMessage}</code>
-              </Td>
-              <Td style={{ fontSize: '12px' }}>
-                <Center>{heartbeat.responseTime} ms</Center>
-              </Td>
-            </Tr>
-          ))
-        ) : (
-          <Tr key={'no-recent-heartbeats'}>
-            <Td colspan="4">
-              <Center>No recent heartbeats</Center>
-            </Td>
-          </Tr>
-        )}
-      </Tbody>
-      <Tfoot>
-        <Tr>
-          <Th>Status</Th>
-          <Th>Time</Th>
-          <Th>Status Message</Th>
-          <Th isNumeric>Response Time</Th>
-        </Tr>
-      </Tfoot>
-    </Table>
+
+  const data = thisMonitor.heartbeats
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Status',
+        accessor: 'status',
+        Cell: (cellProps) => {
+          if (cellProps.value === 'UP') {
+            return (
+              <FontAwesomeIcon
+                {...cellProps}
+                icon={faCheck}
+                style={{ color: 'green', marginLeft: '10px' }}
+              />
+            )
+          } else {
+            return (
+              <FontAwesomeIcon
+                {...cellProps}
+                icon={faTimes}
+                style={{ color: 'red', marginLeft: '10px' }}
+              />
+            )
+          }
+        },
+      },
+      {
+        Header: 'Time',
+        accessor: 'createdAt',
+        Cell: (cellProps) => {
+          return moment(cellProps.value).fromNow()
+        },
+      },
+      {
+        Header: 'Status Message',
+        accessor: 'statusMessage',
+        Cell: (cellProps) => {
+          return <code>{cellProps.value}</code>
+        },
+      },
+      {
+        Header: 'Response Time',
+        accessor: 'responseTime',
+        isNumeric: true,
+        Cell: (cellProps) => {
+          return <Center>{cellProps.value} ms</Center>
+        },
+      },
+    ],
+    []
   )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 5 },
+    },
+    useSortBy,
+    usePagination
+  )
+
+  return (
+    <div>
+      <Table {...getTableProps()} size="sm" variant="simple">
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                >
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row, key) => {
+            prepareRow(row)
+            return (
+              <Tr {...row.getRowProps()} key={key}>
+                {row.cells.map((cell) => (
+                  <Td
+                    {...cell.getCellProps()}
+                    isNumeric={cell.column.isNumeric}
+                  >
+                    {cell.render('Cell')}
+                  </Td>
+                ))}
+              </Tr>
+            )
+          })}
+        </Tbody>
+        <Tfoot>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isNumeric={column.isNumeric}
+                >
+                  {column.render('Header')}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Tfoot>
+      </Table>
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <Input
+            type="number"
+            size="xs"
+            placeholder="page"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>
+        <div style={{ width: '100px', float: 'right' }}>
+          <Select
+            size="xs"
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[5, 10, 25, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </div>
+  )
+
+  // return (
+  //   <Table size="sm" variant="simple">
+  //     <Thead>
+  //       <Tr>
+  //         <Th>Status</Th>
+  //         <Th>Time</Th>
+  //         <Th>Status Message</Th>
+  //         <Th isNumeric style={{ width: '150px' }}>
+  //           Response Time
+  //         </Th>
+  //       </Tr>
+  //     </Thead>
+  //     <Tbody>
+  //       {heartbeats.length > 0 ? (
+  //         heartbeats.map((heartbeat, key) => (
+  //           <Tr key={key}>
+  //             <Td style={{ paddingLeft: '27px', width: '15px' }}>
+  //               {heartbeat.status === 'UP' ? (
+  //                 <FontAwesomeIcon icon={faCheck} style={{ color: 'green' }} />
+  //               ) : (
+  //                 <FontAwesomeIcon icon={faTimes} style={{ color: 'red' }} />
+  //               )}
+  //             </Td>
+  //             <Td style={{ fontSize: '12px' }}>
+  //               {moment(heartbeat.createdAt).fromNow()}
+  //             </Td>
+  //             <Td>
+  //               <code>{heartbeat.statusMessage}</code>
+  //             </Td>
+  //             <Td style={{ fontSize: '12px' }}>
+  //               <Center>{heartbeat.responseTime} ms</Center>
+  //             </Td>
+  //           </Tr>
+  //         ))
+  //       ) : (
+  //         <Tr key={'no-recent-heartbeats'}>
+  //           <Td colspan="4">
+  //             <Center>No recent heartbeats</Center>
+  //           </Td>
+  //         </Tr>
+  //       )}
+  //     </Tbody>
+  //     <Tfoot>
+  //       <Tr>
+  //         <Th>Status</Th>
+  //         <Th>Time</Th>
+  //         <Th>Status Message</Th>
+  //         <Th isNumeric>Response Time</Th>
+  //       </Tr>
+  //     </Tfoot>
+  //   </Table>
+  // )
 }
 
 export default MonitorHeartbeats
