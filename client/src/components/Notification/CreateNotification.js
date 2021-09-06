bc9a0dd333710a281ef56938aabf8b3f66f2e7db
bsord <brandon.sorgdrager@gmail.com>
2021-09-05 19:44:09 -0500

    Functional form validation

--- client/src/components/Notification/CreateNotification.js
+++ client/src/components/Notification/CreateNotification.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { Button, Input, Select } from '@chakra-ui/react'
 
 import { NotificationContext } from '../../contexts/NotificationContext'
@@ -8,8 +8,7 @@ import isValidEmail from '../../util/isValidEmail'
 
 function CreateNotification() {
   const { createNotification, loading } = useContext(NotificationContext)
-
-  let [notificationInfo, setNotificationInfo] = useState({
+  const initNotificationInfo = {
     name: '',
     type: '',
     config: {
@@ -20,20 +19,30 @@ function CreateNotification() {
       mailPass: '',
       mailHost: '',
     },
-  })
+  }
 
-  const [invalidName, isInvalidName] = React.useState(null)
-  const [invalidType, isInvalidType] = React.useState(null)
-  const [invalidSlack, isInvalidSlack] = React.useState(null)
-  const [invalidMailTo, isInvalidMailTo] = React.useState(null)
-  const [invalidMailFrom, isInvalidMailFrom] = React.useState(null)
-  const [invalidMailHost, isInvalidMailHost] = React.useState(null)
-  const [invalidMailUsername, isInvalidMailUsername] = React.useState(null)
-  const [invalidMailPassword, isInvalidMailPassword] = React.useState(null)
+  const initFormValidation = {
+    formErrors: {},
+    nameValid: null,
+    typeValid: null,
+    slackWebhookValid: null,
+    mailToValid: null,
+    mailFromValid: null,
+    mailHostValid: null,
+    mailUsernameValid: null,
+    mailPassValid: null
+  }
+
+  let [notificationInfo, setNotificationInfo] = useState(initNotificationInfo)
+
+  let [formValidation, setFormValidation] = useState(initFormValidation)
+
+  let [formValid, setFormValid] = useState(false)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target
     setNotificationInfo({ ...notificationInfo, [name]: value })
+    validateField(name, value)
   }
 
   const handleKeyDown = (event) => {
@@ -42,171 +51,109 @@ function CreateNotification() {
     }
   }
 
-  const verifyForm = () => {
-    // Name
-    if (notificationInfo.name && notificationInfo.name.length > 1) {
-      isInvalidName(false)
-    } else isInvalidName(true)
-
-    // Type
-    if (notificationInfo.type && notificationInfo.type.length > 1) {
-      isInvalidType(false)
-    } else isInvalidType(true)
-
-    // Slack
-    if (
-      notificationInfo &&
-      notificationInfo.type === 'slack' &&
-      notificationInfo.config &&
-      notificationInfo.config.slackWebhook &&
-      notificationInfo.config.slackWebhook.length > 0
-    ) {
-      // Check this is a real url
-      if (isValidUrl(notificationInfo.config.slackWebhook)) {
-        isInvalidSlack(false)
-      } else {
-        isInvalidSlack(true)
-      }
-    } else isInvalidSlack(true)
-
-    // MailTo
-    if (
-      notificationInfo &&
-      notificationInfo.type === 'email' &&
-      notificationInfo.config &&
-      notificationInfo.config.mailTo &&
-      notificationInfo.config.mailTo.length > 1
-    ) {
-      // Check this is a real email
-      if (isValidEmail(notificationInfo.config.mailTo)) {
-        isInvalidMailTo(false)
-      } else {
-        isInvalidMailTo(true)
-      }
-    } else {
-      isInvalidMailTo(true)
-    }
-
-    // MailFrom
-    if (
-      notificationInfo &&
-      notificationInfo.type === 'email' &&
-      notificationInfo.config &&
-      notificationInfo.config.mailFrom &&
-      notificationInfo.config.mailFrom.length > 0
-    ) {
-      // Check this is a real email
-      if (isValidEmail(notificationInfo.config.mailFrom)) {
-        isInvalidMailFrom(false)
-      } else {
-        isInvalidMailFrom(true)
-      }
-    } else {
-      isInvalidMailFrom(true)
-    }
-
-    // MailHost
-    if (notificationInfo.config.mailHost && notificationInfo.config.mailHost.length > 1) {
-      isInvalidMailHost(false)
-    } else isInvalidMailHost(true)
-
-    // MailUsername
-    if (notificationInfo.config.mailUsername && notificationInfo.config.mailUsername.length > 1) {
-      isInvalidMailUsername(false)
-    } else isInvalidMailUsername(true)
-
-    // MailPassword
-    if (notificationInfo.config.mailPass && notificationInfo.config.mailPass.length > 1) {
-      isInvalidMailPassword(false)
-    } else isInvalidMailPassword(true)
-
-    if (!invalidName && !invalidType) {
-      switch(notificationInfo.type){
-        case "slack":
-          if(!invalidSlack){
-            return true
-          } else {
-            return false
-          }
-        case "email":
-          if(!invalidMailTo && !invalidMailFrom && !invalidMailHost && !invalidMailUsername && !invalidMailPassword){
-            return true
-          } else {
-            return false
-          }
-        default:
-          return false
-      }
-
-    } else {
-      return false
-    }
-  }
-
   const handleConfigChange = (event) => {
     const { name, value } = event.target
     const oldConfig = notificationInfo.config
     const newConfig = {...oldConfig, [name]: value}
     setNotificationInfo({ ...notificationInfo, config: newConfig })
+    validateField(name, value)
   }
 
   const handleClear = () => {
-    notificationInfo = setNotificationInfo({
-      name: '',
-      type: '',
-      config: {
-        slackWebhook: '',
-        mailTo: '',
-        mailFrom: '',
-        mailUsername: '',
-        mailPass: '',
-        mailHost: '',
-      },
-    })
+    setNotificationInfo(initNotificationInfo)
+    setFormValidation(initFormValidation)
   }
   
   const handleCreateNotification = () => {
-    if (verifyForm() && !invalidName && !invalidType) {
-      if (
-        notificationInfo.type === 'email' &&
-        notificationInfo.config.mailTo.length > 1 &&
-        invalidMailTo === false
-      ) {
-        createNotification(notificationInfo, (result) => {
-          if (result.status === 'success') {
-            handleClear()
-            // find a way to close the drawer?
-          }
-        })
-      } else if (
-        notificationInfo.type === 'slack' &&
-        notificationInfo.config.slackWebhook.length > 0 &&
-        invalidSlack === false
-      ) {
-        createNotification(notificationInfo, (result) => {
-          if (result.status === 'success') {
-            handleClear()
-            // find a way to close the drawer?
-          }
-        })
-      } else {
-        setTimeout(() => {
-          isInvalidName(false)
-          isInvalidType(false)
-          isInvalidSlack(false)
-          isInvalidMailTo(false)
-        }, 1200)
-      }
+    if(formValid){
+      createNotification(notificationInfo, (result) => {
+        if (result.status === 'success') {
+          handleClear()
+          // find a way to close the drawer?
+        }
+      })
     } else {
-      setTimeout(() => {
-        isInvalidName(false)
-        isInvalidType(false)
-        isInvalidSlack(false)
-        isInvalidMailTo(false)
-      }, 1200)
+      // toast here
+      console.log("you must fill out the form properly")
     }
   }
 
+  const validateField = (fieldName, value) => {
+    // get existing form errors
+    let newFormValidation = formValidation
+  
+    // update validation errors 
+    switch(fieldName) {
+      case 'name':
+        newFormValidation.nameValid = value.length >= 1;
+        newFormValidation.formErrors.name = newFormValidation.nameValid ? '' : ' is too short';
+        break;
+      case 'type':
+        newFormValidation.typeValid = value.length >= 1;
+        newFormValidation.formErrors.type = newFormValidation.nameValid ? '' : ' you must select a notification type';
+        break;
+      case 'slackWebhook':
+        newFormValidation.slackWebhookValid = isValidUrl(value)
+        newFormValidation.formErrors.slackWebhook = newFormValidation.slackWebhookValid ? '' : ' you must select a notification type';
+        break;
+      case 'mailTo':
+        newFormValidation.mailToValid = isValidEmail(value)
+        newFormValidation.formErrors.mailTo = newFormValidation.mailToValid ? '' : ' you must enter a valid email';
+        break;
+      case 'mailFrom':
+        newFormValidation.mailFromValid = isValidEmail(value)
+        newFormValidation.formErrors.mailFrom = newFormValidation.mailFromValid ? '' : ' you must enter a valid email';
+        break;
+      case 'mailHost':
+        newFormValidation.mailHostValid = value.length >= 1;
+        newFormValidation.formErrors.mailHost = newFormValidation.mailHostValid ? '' : ' is too short';
+        break;
+      case 'mailUsername':
+        newFormValidation.mailUsernameValid = value.length >= 1;
+        newFormValidation.formErrors.mailUsername = newFormValidation.mailUsernameValid ? '' : ' is too short';
+        break;
+      case 'mailPass':
+        newFormValidation.mailPassValid = value.length >= 1;
+        newFormValidation.formErrors.mailPass = newFormValidation.mailPassValid ? '' : ' is too short';
+        break;
+      default:
+        break;
+    }
+
+    setFormValidation({
+      ...formValidation, ...newFormValidation
+    });
+
+  }
+
+  const validateForm = () => {
+    // validate for when slack is chose
+    console.log(formValidation)
+    console.log(formValid)
+    if(formValidation.nameValid && formValidation.typeValid && notificationInfo.type === 'slack'){
+      if(formValidation.slackWebhookValid){
+        setFormValid(true)
+      } else {
+        setFormValid(false)
+      }
+    // validate for when email is chosen
+    } else if (formValidation.nameValid && formValidation.typeValid && notificationInfo.type === 'email'){
+      if(formValidation.mailToValid && formValidation.mailFromValid && formValidation.mailHostValid && formValidation.mailUsernameValid && formValidation.mailPassValid ){
+        setFormValid(true)
+      } else {
+        setFormValid(false)
+      }
+    } else {
+      setFormValid(false)
+    }
+  }
+
+  useEffect(() => {
+    validateForm()
+    //eslint-disable-next-line
+  }, [formValidation])
+  
+  console.log(formValidation.nameValid)
   return (
     <div>
       <div>
@@ -221,15 +168,16 @@ function CreateNotification() {
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
         name="name"
-        isInvalid={invalidName}
+        isInvalid={!formValidation.nameValid && formValidation.nameValid !== null}
       />
+      <div>{formValidation.nameValid}</div>
 
       <Select
         placeholder="Notification Type"
         value={notificationInfo.type}
         onChange={handleInputChange}
         name="type"
-        isInvalid={invalidType}
+        isInvalid={!formValidation.typeValid && formValidation.typeValid !== null}
       >
         <option value="email">
           Email
@@ -246,7 +194,7 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="slackWebhook"
-          isInvalid={invalidSlack}
+          isInvalid={!formValidation.slackWebhookValid && formValidation.slackWebhookValid !== null}
         />
       ) : null}
 
@@ -259,7 +207,7 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="mailTo"
-          isInvalid={invalidMailTo}
+          isInvalid={!formValidation.mailToValid && formValidation.mailToValid !== null}
         />
       ) : null}
       {notificationInfo.type && notificationInfo.type === 'email' ? (
@@ -271,7 +219,7 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="mailFrom"
-          isInvalid={invalidMailFrom}
+          isInvalid={!formValidation.mailFromValid && formValidation.mailFromValid !== null}
         />
       ) : null}
       {notificationInfo.type && notificationInfo.type === 'email' ? (
@@ -283,7 +231,7 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="mailHost"
-          isInvalid={invalidMailHost}
+          isInvalid={!formValidation.mailHostValid && formValidation.mailHostValid !== null}
         />
       ) : null}
       {notificationInfo.type && notificationInfo.type === 'email' ? (
@@ -295,7 +243,7 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="mailUsername"
-          isInvalid={invalidMailUsername}
+          isInvalid={!formValidation.mailUsernameValid && formValidation.mailUsernameValid !== null}
         />
       ) : null}
       {notificationInfo.type && notificationInfo.type === 'email' ? (
@@ -307,12 +255,12 @@ function CreateNotification() {
           onChange={handleConfigChange}
           onKeyDown={handleKeyDown}
           name="mailPass"
-          isInvalid={invalidMailPassword}
+          isInvalid={!formValidation.mailPassValid && formValidation.mailPassValid !== null}
         />
       ) : null}
 
       <div style={{ marginTop: '10px' }}>
-        <Button
+        <Button disabled={!formValid}
           onClick={handleCreateNotification}
           variant="solid"
           colorScheme="purple"
