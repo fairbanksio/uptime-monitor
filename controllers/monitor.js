5ae5b12038840d1c7236fa623df515e71a8e876b
bsord <brandon.sorgdrager@gmail.com>
2021-09-21 11:57:46 -0500

    Fix ordering of results when returned from update call

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,28 +1,28 @@
-let Monitor = require('../models/monitor')
-var monitoringService = require('../services/monitoring')
+let Monitor = require("../models/monitor");
+var monitoringService = require("../services/monitoring");
 
 // Get all monitors
 exports.getAll = (req, res, next) => {
   Monitor.find({ owner: req.user._id })
-    .collation({ locale: 'en' })
+    .collation({ locale: "en" })
     .sort({ name: 1 })
     .populate({
-      path: 'events heartbeats',
+      path: "events heartbeats",
       options: { sort: { createdAt: -1 } },
     })
-    .slice('heartbeats', -20)
+    .slice("heartbeats", -20)
     .then((monitors) => {
-      res.json(monitors)
+      res.json(monitors);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+      res.status(422).send(err.errors);
+    });
+};
 
 // Create monitor
 exports.create = (req, res, next) => {
-  const { name, interval, enabled, type, config, notifications } = req.body
-  const { user } = req
+  const { name, interval, enabled, type, config, notifications } = req.body;
+  const { user } = req;
   var newMonitor = new Monitor({
     name: name,
     interval: interval,
@@ -31,73 +31,78 @@ exports.create = (req, res, next) => {
     config: config,
     owner: user._id,
     notifications: notifications,
-  })
+  });
   newMonitor
     .save()
     .then((monitor) => {
-      monitoringService.startMonitor(monitor)
-      res.json(monitor)
+      monitoringService.startMonitor(monitor);
+      res.json(monitor);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+      res.status(422).send(err.errors);
+    });
+};
 
 // Read one monitor
 exports.getOne = (req, res, next) => {
   Monitor.findOne({ _id: req.params.monitorId, owner: req.user._id })
     .then((monitor) => {
-      res.json(monitor)
+      res.json(monitor);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+      res.status(422).send(err.errors);
+    });
+};
 
 // Update one monitor
 exports.update = (req, res, next) => {
   const updatedMonitor = (query) => ({
-    ...(query.hasOwnProperty('name') && { name: query.name }),
-    ...(query.hasOwnProperty('interval') && { interval: query.interval }),
-    ...(query.hasOwnProperty('enabled') && { enabled: query.enabled }),
-    ...(query.hasOwnProperty('config') && { config: query.config }),
-    ...(query.hasOwnProperty('owner') && { owner: query.owner }),
-    ...(query.hasOwnProperty('notifications') && { notifications: query.notifications }),
-  })
+    ...(query.hasOwnProperty("name") && { name: query.name }),
+    ...(query.hasOwnProperty("interval") && { interval: query.interval }),
+    ...(query.hasOwnProperty("enabled") && { enabled: query.enabled }),
+    ...(query.hasOwnProperty("config") && { config: query.config }),
+    ...(query.hasOwnProperty("owner") && { owner: query.owner }),
+    ...(query.hasOwnProperty("notifications") && {
+      notifications: query.notifications,
+    }),
+  });
 
   Monitor.findByIdAndUpdate(
     { _id: req.params.monitorId, owner: req.user._id },
     updatedMonitor(req.body),
     { new: true }
   )
-    .populate('events heartbeats')
-    .slice('heartbeats', -20)
+    .populate({
+      path: "events heartbeats",
+      options: { sort: { createdAt: -1 } },
+    })
+    .slice("heartbeats", -20)
     .then((monitor) => {
-      monitoringService.updateMonitor(monitor)
-      res.json(monitor)
+      monitoringService.updateMonitor(monitor);
+      res.json(monitor);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+      res.status(422).send(err.errors);
+    });
+};
 
 // Delete one monitor
 exports.delete = (req, res, next) => {
   // delete doesn't return an object id and we need it to stop monitor
   Monitor.findOne({ _id: req.params.monitorId, owner: req.user._id })
     .then((monitor) => {
-      monitoringService.stopMonitor(monitor)
+      monitoringService.stopMonitor(monitor);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
+      res.status(422).send(err.errors);
+    });
 
   // delete monitor
   Monitor.deleteOne({ _id: req.params.monitorId })
     .then((deleteResult) => {
-      res.json(deleteResult)
+      res.json(deleteResult);
     })
     .catch((err) => {
-      res.status(422).send(err.errors)
-    })
-}
+      res.status(422).send(err.errors);
+    });
+};
